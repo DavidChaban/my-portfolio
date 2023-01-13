1751d1f1951126401358dc34f9697162419c9441
David Chaban <davidchaban01@gmail.com>
2023-01-13 03:21:00 -0300

    Se agrega home1

---
+++ src/components/Home1.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import '../styles/home1.css';
+
+
+
+
+const Home1 = () => {
+
+    const tittle = "Bienvenidos";
+    const slogan = "a mi portfolio"
+
+
+
+  return (
+   
+
+    
+    <div className='home-container'>
+
+    <h1 className='tittle-home'><span className='my-style'> {tittle}  </span>   </h1>
+        
+    <p className='slogan-home'> {slogan}</p>
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+   
+
+
+  )
+}
+
+export default Home1
