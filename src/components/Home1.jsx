553d404634a7922a4f4a5b50d44360cf45509f6d
David Chaban <davidchaban01@gmail.com>
2023-01-15 20:33:03 -0300

    Se agregan  recursos

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import '../styles/home1.css';
-
-
+import { ReactDOM } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const Home1 = () => {
 
     const tittle = "Bienvenidos";
     const slogan = "a mi portfolio"
+  
+
+
+
 
 
 
@@ -16,12 +20,15 @@ const Home1 = () => {
 
     
     <div className='home-container'>
+      <div className='home1'>
+    <div className='home2'>
 
     <h1 className='tittle-home'><span className='my-style'> {tittle}  </span>   </h1>
-        
     <p className='slogan-home'> {slogan}</p>
 
+    </div>
 
+      </div>
 
 
 
@@ -34,8 +41,8 @@ const Home1 = () => {
     </div>
 
 
-
-
+    
+   
 
 
    
