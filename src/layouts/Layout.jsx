f252b8d540f77210b768395eb9cd67dd28eede58
David Chaban <davidchaban01@gmail.com>
2023-01-16 23:20:58 -0300

    Se agrega Layout

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header/Header"
+// import Footer from "../components/Footer"
+
+
+
+function Layout(props) {
+    return (
+        <>
+        
+            <Header />
+            {props.children}
+            {/* <Footer/> */}
+      
+
+
+        </>
+    )
+}
+export default Layout
