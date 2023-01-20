e2dff19d1985e674479ef0b828233f0a60f2523f
David Chaban <davidchaban01@gmail.com>
2023-01-19 22:54:26 -0300

    Se agregan links

--- src/components/Home/Home1.jsx
+++ src/components/Home/Home1.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../Home/home1.css';
 import 'typeface-quicksand';
-
+// import Aboutme from "../Aboutme/Aboutme"
 const Home1 = () => {
     const tittle = "DAVID CHABAN";
     const slogan = "Frontend Developer"
   return (
+    <>
+
+    
     <div className='home-container'>
       <div className='home1'>
     <div className='home2'>
@@ -15,33 +18,25 @@ const Home1 = () => {
     <h1 className='tittle-home'><span className='my-style'> {tittle}  </span>   </h1>
     <p className='slogan-home'> {slogan}</p>
     <div class="redes">
-                <a href="https://github.com/DavidChaban"><i className="fa-brands fa-github"></i></a>
-                <a href="https://twitter.com/tdcdev"><i className="fa-brands fa-twitter"></i></a>
-                <a href="https://www.instagram.com/tendodc/"><i class="fa-brands fa-instagram"></i></a>
-                <a href="https://www.linkedin.com/in/daviidc"><i className="fa-brands fa-linkedin-in"></i></a>
+                <a target={"_blank"} href="https://github.com/DavidChaban"><i className="fa-brands fa-github"></i></a>
+                <a target={"_blank"} href="https://twitter.com/tdcdev"><i className="fa-brands fa-twitter"></i></a>
+                <a target={"_blank"} href="https://www.instagram.com/tendodc/"><i class="fa-brands fa-instagram"></i></a>
+                <a target={"_blank"} href="https://www.linkedin.com/in/daviidc"><i className="fa-brands fa-linkedin-in"></i></a>
             </div>
+       
    </div>
     </div>
 
-    <section> </section>
-
-
-
-
-
-
-
-
-
-
 
     </div>
    
+    <div>
 
-
+    {/* <Aboutme/> */}
    
+    </div>
 
-
+    </>
   )
 }
 
