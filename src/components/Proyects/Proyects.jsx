fe30c50290007680f0522446eec0e0b559d71d89
David Chaban <davidchaban01@gmail.com>
2023-01-31 22:57:32 -0300

    se agregan proyectos

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -9,14 +9,44 @@ const Proyects = () => {
     <div className='pr-container'>
 
     <div className='box-container'>
-   
+
     
-    <div className='proyect1'> Mighty Dugs </div>
-    <div className='proyect2'>Pet shop</div>
+    <div className='proyect1'> 
+    
+      <a href='https://challenger-one-david-chaban.vercel.app/' target="_blank" rel="noopener noreferrer"><button  className='demo-button'>Demo</button> </a>
+        <a href='https://github.com/DavidChaban/ChallengerOne-DavidChaban'target="_blank" rel="noopener noreferrer"><button className='code-button'>Code</button></a>
+
+
+ </div>
+
+
+
+
+
+    <div className='proyect2'>
+    <a href='https://mindy-pet-shop.vercel.app/'target="_blank" rel="noopener noreferrer"><button className='demo-button'>Demo</button>  </a>
+    <a href='https://github.com/DavidChaban/Mindy-PetShop' target="_blank" rel="noopener noreferrer"><button className='code-button'>Code</button>  </a>
+    
+    
+    
+    </div>
+
     <div className='proyect3'>Amazing Events</div>
+
     <div className='proyect4'>My Tinerary</div>
-    <div className='proyect5'>Clothes Football retro</div>
+
+    <div className='proyect5'>
+    <a href='https://rftc-web.vercel.app/' target="_blank" rel="noopener noreferrer">
+    <button className='demo-button'>Demo</button> 
+    </a>
+    <a href='https://github.com/nehuen-aumedes-diez/Mindhub-Challengue-RFTC-Front' target="_blank" rel="noopener noreferrer">
+    <button className='code-button'>Code</button> 
+    </a>
+
+    </div>
+
     <div className='proyect6'>Todo List</div>
+
     
     </div>
     
