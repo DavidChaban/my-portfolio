210c1c9d3ecf1f597cd4bbee9805c4dcdbb633d4
David Chaban <davidchaban01@gmail.com>
2023-02-03 00:43:27 -0300

    Se realizan cambios en el responsive del header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../Header/Header.css";
-import { Link as LinkRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Burguer() {
   const btn1 = "HOME";
@@ -17,29 +17,29 @@ export default function Burguer() {
     <div className="nav-container1"> 
       <div className="nav-container">
         <div>
-          <a href="/">
+          <Link to="/home">
             <button className="nav-5">{btn5}</button>
-          </a>
+          </Link>
         </div>
         
         <div className="nav-0">
 
 
-          <LinkRouter to="/">
+          <Link to="/home">
             <button className="nav-1">{btn1}</button>
-          </LinkRouter>
+          </Link>
 
-          <LinkRouter to="/aboutme">
+          <Link to="/aboutme">
             <button className="nav-2"> {btn2}</button>
-          </LinkRouter>
+          </Link>
 
-          <LinkRouter to="/proyects">
+          <Link to="/proyects">
             <button className="nav-2"> {btn3}</button>
-          </LinkRouter>
+          </Link>
 
-          <LinkRouter to="/contact">
+          <Link to="/contact">
             <button className="nav-2"> {btn4}</button>
-          </LinkRouter>
+          </Link>
 
 
         </div>
@@ -50,25 +50,25 @@ export default function Burguer() {
               <div className="burguer-container">
                 <i class="fa-sharp fa-solid fa-xmark x-awesome" onClick={hide}></i>
                <div className="links-header">
-                <LinkRouter to="#home" className="a1">
+                <Link to="/home" className="a1">
                   {btn1}
                   <button className="btn-burguer"> </button>
-                </LinkRouter>
+                </Link>
 
-                <LinkRouter to="#aboutme" className="a1">
+                <Link to="/aboutme" className="a1">
                   {btn2}
                   <button className="btn-burguer"> </button>
-                </LinkRouter>
+                </Link>
 
-                <LinkRouter to="#proyects" className="a1">
+                <Link to="/proyects" className="a1">
                   {btn3}
                   <button className="btn-burguer"> </button>
-                </LinkRouter>
+                </Link>
 
-                <LinkRouter to="/#contact" className="a1">
+                <Link to="/contact" className="a1">
                   {btn4}
                   <button className="btn-burguer"> </button>
-                </LinkRouter>
+                </Link>
                 </div>
               </div>
             </>
