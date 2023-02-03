0405c86c3a5d296907b94d2e50b161406255284d
David Chaban <davidchaban01@gmail.com>
2023-02-03 00:53:07 -0300

    Se agrega Ruta de Contact

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Layout from "./layouts/Layout";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Aboutme from "./components/Aboutme/Aboutme"
 import Proyects from "./components/Proyects/Proyects";
+import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <Layout>
         <Routes>
-          <Route path="/home" element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
           <Route path='/app' element={<Layout />}></Route>
           <Route path='/aboutme' element={<Aboutme />}></Route>    
-          <Route path='/proyects' element={<Proyects />}></Route>    
+          <Route path='/proyects' element={<Proyects />}></Route>   
+          <Route path='/contact' element={<Contact/>}></Route>
           {/* <Route path='/aboutme' element={<Aboutme />}></Route>     */}
 
 
