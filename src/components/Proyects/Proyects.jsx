4f3f43f741b342caf8b5b85fa57cda169085e856
David Chaban <davidchaban01@gmail.com>
2023-02-13 18:37:24 -0300

    se agrega app

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -4,27 +4,41 @@ import "../Proyects/Proyects.css";
 
 const Proyects = () => {
   return (
+    <div className="pr01container">
     <div className="pr-container">
       {/*  CARD 1  */}
       <div className="box-container">
         <div className="card-container1">
-          <h2 className="project-h2">Encriptador</h2>
+          <h2 className="project-h2">
+Encrypter</h2>
           <div className="proyect1">
-            <p className="proyect-text"> App de Encriptado, realizada con Js</p>
-            <a
+            <a className="a-pr1"
               href="https://challenger-one-david-chaban.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="demo-button">Demo</button>{" "}
+           
+            <p className="proyect-text"> View Demo</p>
+
+              {/* <button className="demo-button">Demo</button>{" "} */}
             </a>
-            <a
+  
+            <a className="a-pr1"
               href="https://github.com/DavidChaban/ChallengerOne-DavidChaban"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="code-button">Code</button>
+         
+              <button className="code-button">Code 
+              
+                <img src="/github (1).png" className="githubpr"></img>
+              </button>
+
             </a>
+           
+
+
+
           </div>
         </div>
 
@@ -35,45 +49,54 @@ const Proyects = () => {
           <div className="proyect2">
             <p className="proyect-text2">
               {" "}
-              Proyecto Grupal,Realizada con con JS
+              View Demo
             </p>
-            <a
+            <a 
               href="https://mindy-pet-shop.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="demo-button2">Demo</button>{" "}
             </a>
-            <a
+            <a className="a-pr1"
               href="https://github.com/DavidChaban/Mindy-PetShop"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="code-button2">Code</button>{" "}
+              <button className="code-button2">Code
+              
+              <img src="/github (1).png" className="githubpr"></img>
+              
+              </button>{" "}
             </a>
           </div>
         </div>
 
         {/*  CARD 3  */}
-        <div>
+        {/* <div className="card-container5">
         <h2 className="proyect5-h2">RFC </h2>
           <div className="proyect5">
+          <p className="proyect-text5">View Demo</p>
             <a
               href="https://rftc-web.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="demo-button">Demo</button>
+            
             </a>
-            <a
+            
+            <a className="a-pr1"
               href="https://github.com/nehuen-aumedes-diez/Mindhub-Challengue-RFTC-Front"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="code-button">Code</button>
+              <button className="code-button5">Code
+
+              <img src="/github (1).png" className="githubpr"></img>
+
+              </button>
             </a>
           </div>
-          </div>
+          </div> */}
 
 
 
@@ -84,6 +107,9 @@ const Proyects = () => {
 
       </div>
     </div>
+
+</div>
+
   );
 };
 
