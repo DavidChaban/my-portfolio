9cd3ddc9e754a79197e5434f2efa0f6c8d31d4f1
David Chaban <davidchaban01@gmail.com>
2023-02-17 23:47:53 -0300

    se agrega nuevos diseño

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,58 +1,25 @@
 import React, {useState}  from 'react';
-// import { Formik, Form, Field, ErrorMessage } from 'formik';
 import "../Contact/Contact.css";
 import emailjs from '@emailjs/browser';
-import { useNavigate, useEffect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
-
- 
 const Contact = () => {
   const [data, setData] = useState([])
   const nav = useNavigate()
-  // const form = useRef();
-  // const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
-  // useEffect(() =>{
-
-
-  // }, []);
-
-
-
   const handleInputChange = (e) => {
-
     console.log(e.target.value);
     setData({
       ...data,
       [e.target.name]: e.target.value,
     });
   };
-
-
-
   const sendEmail = (e) => {
     e.preventDefault();
-
-
-
-    
     try{
     emailjs.sendForm('service_fbrjyic','template_f4r7lgg',e.target,'Jz0s4jRmxXuNIKG0o')
-
-      
-
-
-
 	.then(res =>console.log(res))
 		.catch(error => console.log(error))
-
-      
-
-
     e.target.reset()
-  //           }
-
-
-
   console.log(e)
   
   if(emailjs.data.success){
@@ -64,38 +31,30 @@ const Contact = () => {
       showConfirmButton: true,
       timer: 1500
     })
-     
-    }
-      
-
+         }
 }catch(error){
   console.log(error)
-  Swal.fire( "Error in Sent",error.res.data.message.join("\n"))
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Message Sent Successfuly',
+    showConfirmButton: true,
+    timer: 3000
+  })
 }
- 
 }
-
-
-
-
-
-
-
   return (
     <div className='div-form'>
       <form className='form-mail' onSubmit={sendEmail}>
    
     <div className='cp'>
       <h2 className='title-form'>Contact Me</h2>
-
       <label for='user_name'>Name: </label>
         <input type="text" name='user_name' id='user_name' placeholder='Name*'  onChange={handleInputChange} required />
         <hr />
-
         <label for="user_email">Email: </label>
         <input type="email" name='user_email' id='user_email' placeholder='Email*'  onChange={handleInputChange} required />
         <hr />
-
         <label for="user_message">Message </label>
         <textarea name="user_message" id="user_message" cols="30" rows="10"  placeholder='Message*' required ></textarea>
         <hr />
@@ -105,8 +64,6 @@ const Contact = () => {
     </div>
   )
   }
-
-
 export default Contact;
 
 
