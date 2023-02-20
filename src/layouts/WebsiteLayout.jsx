6d123f2b96f5c913083cc33d43865abd34ee8353
David Chaban <davidchaban01@gmail.com>
2023-02-19 23:41:00 -0300

    Se corrije error

--- src/layouts/WebsiteLayout.jsx
+++ src/layouts/WebsiteLayout.jsx
@@ -1,16 +1,10 @@
 import Header from "../components/Header/Header"
- 
- 
 
 function WebsiteLayout(props) {
     return (
         <>
-        
             <Header/>
             {props.children}
-           
-
-
         </>
     )
 }
