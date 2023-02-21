eeff46309d30836a9eab0d2a3abaf77cd2f17005
David Chaban <davidchaban01@gmail.com>
2023-02-21 01:01:45 -0300

    se agrega translations a Proyects

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "../Home/Home1";
 import "../Proyects/Proyects.css";
+import { useTranslation } from "react-i18next";
 
 const Proyects = () => {
+  const [t, i18n] = useTranslation("global");
   return (
     <div className="pr01container ">
-    <h2 className="title-myproyects">My Proyects </h2>
+    <h2 className="title-myproyects">{t("proyects.proyects")} </h2>
       <div className="pr-container ">
 
       
         {/*  CARD 1  */}
         <div className="box-container ">
           <div className="card-container1 ">
-            <h2 className="project-h2">Encrypter</h2>
+            <h2 className="project-h2">{t("proyects.Encrypter")}</h2>
             <div className="proyect1 ">
               <a
                 className="a-pr1"
@@ -21,7 +23,7 @@ const Proyects = () => {
                 rel="noopener noreferrer"
               >
                 <div className="container-text">
-                  <p className="proyect-text"> View Demo</p>
+                  <p className="proyect-text"> {t("proyects.ViewDemo")}</p>
                 </div>
               </a>
 
@@ -32,7 +34,7 @@ const Proyects = () => {
                 rel="noopener noreferrer"
               >
                 <button className="code-button">
-                  Code
+                {t("proyects.code")}
                   <img src="/github (1).png" className="githubpr" alt="imagendegithub"></img>
                 </button>
               </a>
@@ -41,7 +43,7 @@ const Proyects = () => {
 
           {/*  CARD 2  */}
           <div className="card-container2">
-            <h2 className="proyect-h02"> Pet Shop Mindy</h2>
+            <h2 className="proyect-h02">{t("proyects.Pet Shop Mindy")}</h2>
             <div className="proyect2">
               <a
                 className="a-pr11"
@@ -50,7 +52,7 @@ const Proyects = () => {
                 rel="noopener noreferrer"
               >
                 <div className="container-text2">
-                  <p className="proyect-text2"> View Demo</p>
+                  <p className="proyect-text2">  {t("proyects.ViewDemo")}</p>
                 </div>
               </a>
               <a
@@ -60,7 +62,7 @@ const Proyects = () => {
                 rel="noopener noreferrer"
               >
                 <button className="code-button2">
-                  Code
+                {t("proyects.code")}
                   <img src="/github (1).png" className="githubpr" alt="imagendeGithub"></img>
                 </button>{" "}
               </a>
@@ -70,7 +72,7 @@ const Proyects = () => {
 
           {/*  CARD 3  */}
           <div className="card-container5">
-            <h2 className="proyect5-h2">RFC </h2>
+            <h2 className="proyect5-h2"> {t("proyects.Retro Football Team Clothing")} </h2>
             <div className="proyect5">
               <a
                 className="a-pr11"
@@ -79,7 +81,7 @@ const Proyects = () => {
                 rel="noopener noreferrer"
               >
                 <div className="container-text5">
-                  <p className="proyect-text5">View Demo</p>
+                  <p className="proyect-text5"> {t("proyects.ViewDemo")}</p>
                 </div>
               </a>
 
@@ -90,7 +92,7 @@ const Proyects = () => {
                 rel="noopener noreferrer"
               >
                 <button className="code-button5">
-                  Code
+                {t("proyects.code")}
                   <img src="/github (1).png" className="githubpr" alt="imagendegithub"></img>
                 </button>
               </a>
